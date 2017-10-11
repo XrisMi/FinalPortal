78ceca3e3a9547b469345f22d2f3537dabc9d844
r1ngs <32281517+r1ngs@users.noreply.github.com>
2017-10-11 12:08:22 -0700

    Added new theme and widgets on second activity

--- app/src/main/java/com/example/buscis_c1_l6/userpasswordsubmit/Login.java
+++ app/src/main/java/com/example/buscis_c1_l6/userpasswordsubmit/Login.java
@@ -10,20 +10,16 @@ import android.view.View;
 public class Login extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+
+
+
+
+}
+
+
 
 }

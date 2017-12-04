0e9e5f06861caf431f8c9e8cd5830ffe5ffaf61e
r1ngs <32281517+r1ngs@users.noreply.github.com>
2017-12-04 12:18:55 -0800

    Second activity now has intent that leads to third activity, ratings bar is working, intent leading to app bio is working. Only thing left is to implement email.

--- app/src/main/java/com/example/buscis_c1_l6/userpasswordsubmit/ThirdPage.java
+++ app/src/main/java/com/example/buscis_c1_l6/userpasswordsubmit/ThirdPage.java
@@ -1,6 +1,6 @@
 package com.example.buscis_c1_l6.userpasswordsubmit;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +10,18 @@ import android.widget.RatingBar;
 import android.widget.RatingBar.OnRatingBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.widget.Toast;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class ThirdPage extends AppCompatActivity {
     private RatingBar ratingBar;
-    private TextView txtRatingValue;
+    private TextView bioView;
     private Button btnSubmit;
-    private Button appButton;
     private Button thankYouButton;
 
     @Override
@@ -24,35 +29,43 @@ public class ThirdPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third_page);
 
-        appButton       = (Button) findViewById(R.id.appButton);
+        bioView = (TextView) findViewById(R.id.bioView);
         thankYouButton  = (Button) findViewById(R.id.thankYouButton);
 
-                addListenerOnRatingBar();
+        addListenerOnRatingBar();
         addListenerOnButton();
 
-        appButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-
-                    Intent i;
-                    i = new Intent(ThirdPage.this, bio.class);
-                    startActivity(i);
-
-            }
+        //String mailto = "mailto:abmukarram@gmail.com" + "&subject=" + Uri.encode("Hello") + "&body =" + Uri.encode("Are you there");
 
 
-        });
-
-        thankYouButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-
-                Intent i;
-                i = new Intent(ThirdPage.this, bio.class);
-                startActivity(i);
-
-            }
+        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
 
 
-        });
+        emailIntent.setData(Uri.parse("mailto:"));
+        String [] to = {"cpmata@gmail.com"};
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT," Sent from my app!");
+        emailIntent.putExtra(Intent.EXTRA_TEXT," Thank you for attending and checking out my app!");
+        emailIntent.setType("message/rfc822");
+
+
+        try {
+            startActivity(emailIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "No Mail App", Toast.LENGTH_LONG).show();
+            System.out.println("Mail Not there");
+        }
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://google.com"));
+
+        try
+        { startActivity(intent);}
+        catch (ActivityNotFoundException e)
+        {Toast.makeText(this, "Cannot Open Browser", Toast.LENGTH_LONG).show();}
+
+
+
 
 
     }
@@ -60,7 +73,7 @@ public class ThirdPage extends AppCompatActivity {
     public void addListenerOnRatingBar() {
 
         ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-        txtRatingValue = (TextView) findViewById(R.id.txtRatingValue);
+
 
         //if rating value is changed,
         //display the current rating value in the result (textview) automatically
@@ -68,7 +81,7 @@ public class ThirdPage extends AppCompatActivity {
             public void onRatingChanged(RatingBar ratingBar, float rating,
                                         boolean fromUser) {
 
-                txtRatingValue.setText(String.valueOf(rating));
+
 
             }
         });
